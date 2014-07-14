31a7b77c70ad4eb867a29dbe42fa64895e3f1c0a
Bogdan Sopilka <hawky20@gmail.com>
2014-07-14 09:08:02 +0300

    begin form validation

--- script.js
+++ script.js
@@ -23,46 +23,10 @@
              }
          });
      }
-//    $('#button').click(function(){
-//        $('#error_message').hide();
-//        if ($('#email').val() != "") {
-//                console.log("Good");
-//                $.cookie("email", $('#email').val(), 30);
-//                //alert($.cookie("email"));
-//                $('#main_page').hide();
-//                $('#drop_page').fadeIn(500);
-//                $("html, body").animate({ scrollTop: 0 }, 600);
-//
-//        } else {
-//                $('#error_message').fadeIn(500);
-//                }
-//        });
-$("select").focus(function(){
-    $(this).parent().find("input:radio").click();
-});
-     //save input value
-//     $("#activate-step-2").click(function(){
-//         // validate insurance
-//         //alert($("input[name=insurance]:checked").val());
-//
-//         get_element_value("insurance");
-//         get_element_value("tdp_insurance");
-//         get_element_value("trauma_insurance");
-//         get_element_value("income_insurance");
-//         get_checkbox_value("generale_insurance");
-//         get_element_value_noselect("cover_exclusions");
-//     });
-//     $("#activate-step-3").click(function(){
-//         // validate insurance
-//         //alert($("input[name=insurance]:checked").val());
-//
-//         get_element_value("debt");
-//         get_element_value_noselect("children");
-//         get_element_value_noselect("family_survive");
-//         get_element_value_noselect("cost_of_living");
-//         get_element_value_noselect("last_time_insurance");
-//         get_element_value_noselect("health_wellness");
-//     });
+
+    $("select").focus(function(){
+        $(this).parent().find("input:radio").click();
+    });
 
  //Steps
      var navListItems = $('ul.setup-panel li a'),
@@ -107,37 +71,27 @@ $("select").focus(function(){
 //         $(this).remove();
      });
      $('#activate-step-3').on('click', function(e) {
-         $('ul.setup-panel li:eq(2)').removeClass('disabled');
-         $('ul.setup-panel li:eq(1)').addClass('disabled');
-         $('ul.setup-panel li a[href="#step-3"]').trigger('click');
-         get_element_value("debt");
-         get_element_value_noselect("children");
-         get_element_value_noselect("family_survive");
-         get_element_value_noselect("cost_of_living");
-         get_element_value_noselect("last_time_insurance");
-         get_element_value_noselect("health_wellness");
-         $("html, body").animate({ scrollTop: 0 }, 600);
+         $("#error_message_no_answer").hide();
+         data["form_error"] = false;
+         get_element_value("debt", "Do you have any debt?");
+         get_element_value_noselect("children", "How many dependent children do you have?");
+         get_element_value_noselect("family_survive", "Would your family or loved ones be able to survive financially without you if you were to pass away?");
+         get_element_value_noselect("cost_of_living", "If you were unable to ever work again, how would you meet your cost of living?");
+         get_element_value_noselect("last_time_insurance", "When was the last time you had your existing insurance or your overall insurance need reviewed?");
+         get_element_value_noselect("health_wellness", "Are you actively participating in a structured Health and Wellness program to assist you in meeting your Health and Wellness goals and to recrease your likelihood of ever needing to claim on insurance for health reasons?");
+         if (data["form_error"] == false) {
+             $('ul.setup-panel li:eq(2)').removeClass('disabled');
+             $('ul.setup-panel li:eq(1)').addClass('disabled');
+             $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+             $("html, body").animate({ scrollTop: 0 }, 600);
+         }
 //         return false;
 //         $(this).remove();
      });
 
-//     function get_element_value(element_name) {
-//         var radio_val = $("input[name=" + element_name + "]:checked").val();
-//         if (radio_val == 1) {
-//             data[element_name] = $("#" + element_name + "_value option:selected").val();
-//         } else if (radio_val == 0) {
-//             data[element_name] = 0;
-//         } else if (radio_val == -1) {
-//             data[element_name] = -1;
-//         } else {
-//             $("html, body").animate({ scrollTop: 0 }, 600);
-//             $('#error_message_no_answer').fadeIn(500);
-//         }
-////         console.log(data);
-//     }
      function get_element_value(element_name, error_description) {
          var radio_val = $("input[name=" + element_name + "]:checked").val();
-         console.log(element_name + "=" + radio_val);
+//         console.log(element_name + "=" + radio_val);
          switch (radio_val) {
              case "1":
                  data[element_name] = $("#" + element_name + "_value option:selected").val();
@@ -150,7 +104,7 @@ $("select").focus(function(){
                  break;
              default :
                  $("html, body").animate({ scrollTop: 0 }, 600);
-                 $('#error_message_no_answer').html("Please answer to question" + error_description).fadeIn(500);
+                 $('#error_message_no_answer').html("Please answer to question " + error_description).fadeIn(500);
                  data["form_error"] = true;
                  break;
          }
@@ -162,51 +116,39 @@ $("select").focus(function(){
          switch (radio_val) {
              case "1":
                  data[element_name] = 1;
-//             data.tdp_insurance = "test1_tdp";
-
                  break;
 
              case "0":
-                 //data = JSON.stringify({element_name:0});
-                 //alert("No");
                  data[element_name] = 0;
-
                  break;
 
              case "-1":
-                 //alert("Not sure");
-                 //data = JSON.stringify({element_name:-1});
                  data[element_name] = -1;
-
                  break;
 
              case "2":
                  data[element_name] = 2;
-
                  break;
 
              case "3":
                  data[element_name] = 3;
-
                  break;
 
              case "4":
                  data[element_name] = 4;
-
                  break;
 
              case "5":
                  data[element_name] = 5;
-
                  break;
 
              case "6":
                  data[element_name] = 6;
-
                  break;
+
              default:
                  $("html, body").animate({ scrollTop: 0 }, 600);
-                 $('#error_message_no_answer').html("Please answer to question" + error_description).fadeIn(500);
+                 $('#error_message_no_answer').html("Please answer to question " + error_description).fadeIn(500);
                  data["form_error"] = true;
 
                  break;
@@ -231,10 +173,8 @@ $("select").focus(function(){
          }
 
      }
-//     $('#activate-step-2').click(function(){
-//         $("html, body").animate({ scrollTop: 0 }, 600);
-//         return false;
-//     });
-     $("#email_form").val($.cookie("email"));
+     $("#email_form")
+         .val($.cookie("email"));
+
 
  });
